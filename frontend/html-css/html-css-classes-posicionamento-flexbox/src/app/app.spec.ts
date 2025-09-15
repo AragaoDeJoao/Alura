f5516f778c082ebbec890fcd0d0c712eed0c5538
Joao Carlos Luzardo de Aragao <joao.aragao@mpt.mp.br>
2025-09-15 15:41:47 -0300

    Iniciando curso html-css-classes-posicionamento-flexbox

--- frontend/html-css/html-css-classes-posicionamento-flexbox/src/app/app.spec.ts
+++ frontend/html-css/html-css-classes-posicionamento-flexbox/src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, html-css-classes-posicionamento-flexbox');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, html-css-ambiente-arquivos-tags');
   });
 });
---
+++ frontend/html-css/html-css-classes-posicionamento-flexbox/src/app/exercicios/exercicios.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicios',
+  imports: [],
+  templateUrl: './exercicios.html',
+  styleUrl: './exercicios.css'
+})
+export class Exercicios {
+
+}
