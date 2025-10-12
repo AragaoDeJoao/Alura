083f88adbf4fbc4be4aa5dfd4b8110e8570b6445
Joao Aragao <joao.c.aragao@gmail.com>
2025-10-11 23:59:34 -0300

    Finalizando o primeiro formulário

--- frontend/angular/freelando-reactive-form/src/app/pages/cadastro-form/cadastro-form.component.ts
+++ frontend/angular/freelando-reactive-form/src/app/pages/cadastro-form/cadastro-form.component.ts
@@ -1,48 +1,73 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {RadioOptionComponent} from '../../shared/components/radio-option/radio-option.component';
+import {ExperienceLevelComponent} from '../../shared/components/experience-level/experience-level.component';
+import {ButtonComponent} from '../../shared/components/button/button.component';
 
 const MODULES = [
   CommonModule,
   ReactiveFormsModule
 ];
 
+const COMPONENTS = [
+  RadioOptionComponent,
+  ExperienceLevelComponent,
+  ButtonComponent,
+]
+
 @Component({
   selector: 'app-cadastro-form',
   standalone: true,
   imports: [
-    ...MODULES
+    ...MODULES,
+    ...COMPONENTS,
   ],
   templateUrl: './cadastro-form.component.html',
   styleUrls: ['./cadastro-form.component.scss']
 })
-export class CadastroFormComponent {
+export class CadastroFormComponent implements OnInit {
   cadastroForm!: FormGroup;
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      areasAtuacao: ['', Validators.required],
+      niveisExperiencia: ['', Validators.required],
+    })
+  }
 
   areasAtuacao = [
-    { id: 'ti', value: 'ti', label: 'TI e Programação' },
-    { id: 'design', value: 'design', label: 'Design e Multimídia' },
-    { id: 'revisao', value: 'revisao', label: 'Revisão' },
-    { id: 'traducao', value: 'traducao', label: 'Tradução' },
-    { id: 'transcricao', value: 'transcricao', label: 'Transcrição' },
-    { id: 'marketing', value: 'marketing', label: 'Marketing' }
+    {id: 'ti', value: 'ti', label: 'TI e Programação'},
+    {id: 'design', value: 'design', label: 'Design e Multimídia'},
+    {id: 'revisao', value: 'revisao', label: 'Revisão'},
+    {id: 'traducao', value: 'traducao', label: 'Tradução'},
+    {id: 'transcricao', value: 'transcricao', label: 'Transcrição'},
+    {id: 'marketing', value: 'marketing', label: 'Marketing'}
   ];
 
   niveisExperiencia = [
-    {
-      id: 'iniciante',
-      label: 'Iniciante',
-      description: '(1 a 3 anos)'
-    },
-    {
-      id: 'intermediario',
-      label: 'Intermediário',
-      description: '(3 a 6 anos)'
-    },
-    {
-      id: 'avancado',
-      label: 'Avançado',
-      description: '(6 anos ou mais)'
-    }
+    {id: 'iniciante', label: 'Iniciante', description: '(1 a 3 anos)'},
+    {id: 'intermediario', label: 'Intermediário', description: '(3 a 6 anos)'},
+    {id: 'avancado', label: 'Avançado', description: '(6 anos ou mais)'}
   ];
+
+
+  onAreaChange(area: string) {
+    this.cadastroForm.get('areasAtuacao')?.setValue(area);
+  }
+
+  onNivelChange(nivel: string) {
+    this.cadastroForm.get('niveisExperiencia')?.setValue(nivel);
+  }
+
+  onProximo() {
+    if (this.cadastroForm.valid) {
+      console.log('Formulário Válido: ', this.cadastroForm.value);
+    }
+  }
+
+  onAnterior() {
+    console.log('Voltar para etapa anterior');
+  }
 }
