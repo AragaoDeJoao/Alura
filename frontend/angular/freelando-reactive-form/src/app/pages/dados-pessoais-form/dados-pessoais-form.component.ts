8710c9aacb2117c52df00c48d7ae3959c849e9d4
Joao Aragao <joao.c.aragao@gmail.com>
2025-10-12 23:34:00 -0300

    Finalizando primeira etapa do curso

--- frontend/angular/freelando-reactive-form/src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ frontend/angular/freelando-reactive-form/src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -1,7 +1,7 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import { ButtonComponent } from '../../shared/components/button/button.component';
+import {ButtonComponent} from '../../shared/components/button/button.component';
 import {Router} from '@angular/router';
 import {CadastroService} from '../../shared/services/cadastro.service';
 
@@ -20,6 +20,36 @@ export class DadosPessoaisFormComponent implements OnInit {
 
   dadosPessoaisForm!: FormGroup;
 
+  estados = [
+    {sigla: 'AC', nome: 'Acre'},
+    {sigla: 'AL', nome: 'Alagoas'},
+    {sigla: 'AP', nome: 'Amapá'},
+    {sigla: 'AM', nome: 'Amazonas'},
+    {sigla: 'BA', nome: 'Bahia'},
+    {sigla: 'CE', nome: 'Ceará'},
+    {sigla: 'DF', nome: 'Distrito Federal'},
+    {sigla: 'ES', nome: 'Espírito Santo'},
+    {sigla: 'GO', nome: 'Goiás'},
+    {sigla: 'MA', nome: 'Maranhão'},
+    {sigla: 'MT', nome: 'Mato Grosso'},
+    {sigla: 'MS', nome: 'Mato Grosso do Sul'},
+    {sigla: 'MG', nome: 'Minas Gerais'},
+    {sigla: 'PA', nome: 'Pará'},
+    {sigla: 'PB', nome: 'Paraíba'},
+    {sigla: 'PR', nome: 'Paraná'},
+    {sigla: 'PE', nome: 'Pernambuco'},
+    {sigla: 'PI', nome: 'Piauí'},
+    {sigla: 'RJ', nome: 'Rio de Janeiro'},
+    {sigla: 'RN', nome: 'Rio Grande do Norte'},
+    {sigla: 'RS', nome: 'Rio Grande do Sul'},
+    {sigla: 'RO', nome: 'Rondônia'},
+    {sigla: 'RR', nome: 'Roraima'},
+    {sigla: 'SC', nome: 'Santa Catarina'},
+    {sigla: 'SP', nome: 'São Paulo'},
+    {sigla: 'SE', nome: 'Sergipe'},
+    {sigla: 'TO', nome: 'Tocantins'}
+  ]
+
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private cadastroService = inject(CadastroService);
@@ -28,14 +58,37 @@ export class DadosPessoaisFormComponent implements OnInit {
     this.dadosPessoaisForm = this.fb.group({
       nomeCompleto: ['', Validators.required],
       estado: ['', Validators.required],
-      cidade:['', Validators.required],
-      email:['', Validators.required],
-      senha:['', Validators.required],
-      repitaSenha: ['', Validators.required],
+      cidade: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmaSenha: ['', Validators.required]
     })
   }
 
-  onAnterior(): void {}
+  onAnterior(): void {
+    this.salvarDadosAtuais();
+    this.router.navigate(['/cadastro/areaAtuacao'])
+  }
 
-  onProximo(): void {}
+  onProximo(): void {
+    if (this.dadosPessoaisForm.valid){
+      this.salvarDadosAtuais();
+      this.router.navigate(['/cadastro/confirmacao']);
+    }
+    else{
+      this.dadosPessoaisForm.markAllAsTouched()
+    }
+  }
+
+  private salvarDadosAtuais(){
+    const formValue = this.dadosPessoaisForm.value;
+    this.cadastroService.updateCadastroData({
+      nomeCompleto: formValue.nomeCompleto,
+      estado: formValue.estado,
+      cidade: formValue.cidade,
+      email: formValue.email,
+      senha: formValue.senha
+    })
+  }
 }
+
