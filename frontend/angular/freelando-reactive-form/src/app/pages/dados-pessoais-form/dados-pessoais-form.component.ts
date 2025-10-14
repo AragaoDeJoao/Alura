251d7ab6ba4c0495cad451a41af377f9f19ea4c8
Joao Carlos Luzardo de Aragao <joao.aragao@mpt.mp.br>
2025-10-14 13:04:12 -0300

    Implementando a logica HTTP no TS

--- frontend/angular/freelando-reactive-form/src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ frontend/angular/freelando-reactive-form/src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -11,6 +11,9 @@ import {
 import {ButtonComponent} from '../../shared/components/button/button.component';
 import {Router} from '@angular/router';
 import {CadastroService} from '../../shared/services/cadastro.service';
+import {BehaviorSubject, Observable, of, startWith, switchMap, tap} from 'rxjs';
+import {Cidade, Estado} from '../../shared/models/ibge.interface';
+import {IbgeService} from '../../shared/services/ibge.service';
 
 @Component({
   selector: 'app-dados-pessoais-form',
@@ -25,44 +28,20 @@ import {CadastroService} from '../../shared/services/cadastro.service';
 })
 
 
-
 export class DadosPessoaisFormComponent implements OnInit {
 
   dadosPessoaisForm!: FormGroup;
 
-  estados = [
-    {sigla: 'AC', nome: 'Acre'},
-    {sigla: 'AL', nome: 'Alagoas'},
-    {sigla: 'AP', nome: 'Amapá'},
-    {sigla: 'AM', nome: 'Amazonas'},
-    {sigla: 'BA', nome: 'Bahia'},
-    {sigla: 'CE', nome: 'Ceará'},
-    {sigla: 'DF', nome: 'Distrito Federal'},
-    {sigla: 'ES', nome: 'Espírito Santo'},
-    {sigla: 'GO', nome: 'Goiás'},
-    {sigla: 'MA', nome: 'Maranhão'},
-    {sigla: 'MT', nome: 'Mato Grosso'},
-    {sigla: 'MS', nome: 'Mato Grosso do Sul'},
-    {sigla: 'MG', nome: 'Minas Gerais'},
-    {sigla: 'PA', nome: 'Pará'},
-    {sigla: 'PB', nome: 'Paraíba'},
-    {sigla: 'PR', nome: 'Paraná'},
-    {sigla: 'PE', nome: 'Pernambuco'},
-    {sigla: 'PI', nome: 'Piauí'},
-    {sigla: 'RJ', nome: 'Rio de Janeiro'},
-    {sigla: 'RN', nome: 'Rio Grande do Norte'},
-    {sigla: 'RS', nome: 'Rio Grande do Sul'},
-    {sigla: 'RO', nome: 'Rondônia'},
-    {sigla: 'RR', nome: 'Roraima'},
-    {sigla: 'SC', nome: 'Santa Catarina'},
-    {sigla: 'SP', nome: 'São Paulo'},
-    {sigla: 'SE', nome: 'Sergipe'},
-    {sigla: 'TO', nome: 'Tocantins'}
-  ]
+
+  estados$!: Observable<Estado[]>;
+  cidade$!: Observable<Cidade[]>;
+
+  carregandoCidades$ = new BehaviorSubject<boolean>(false)
 
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private cadastroService = inject(CadastroService);
+  private ibgeService = inject(IbgeService);
 
   ngOnInit() {
     this.dadosPessoaisForm = this.fb.group({
@@ -73,6 +52,8 @@ export class DadosPessoaisFormComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       confirmaSenha: ['', Validators.required]
     })
+    this.carregarEstados();
+    this.configurarListenerEstado();
   }
 
   onAnterior(): void {
@@ -81,16 +62,15 @@ export class DadosPessoaisFormComponent implements OnInit {
   }
 
   onProximo(): void {
-    if (this.dadosPessoaisForm.valid){
+    if (this.dadosPessoaisForm.valid) {
       this.salvarDadosAtuais();
       this.router.navigate(['/cadastro/confirmacao']);
-    }
-    else{
+    } else {
       this.dadosPessoaisForm.markAllAsTouched()
     }
   }
 
-  private salvarDadosAtuais(){
+  private salvarDadosAtuais() {
     const formValue = this.dadosPessoaisForm.value;
     this.cadastroService.updateCadastroData({
       nomeCompleto: formValue.nomeCompleto,
@@ -100,5 +80,32 @@ export class DadosPessoaisFormComponent implements OnInit {
       senha: formValue.senha
     })
   }
+
+  private carregarEstados() {
+    this.estados$ = this.ibgeService.getEstados();
+  }
+
+  private configurarListenerEstado() {
+    const estadoControl = this.dadosPessoaisForm.get('estado');
+    if (estadoControl) {
+      this.cidade$ = estadoControl.valueChanges.pipe(
+        startWith(''), tap(() => {
+          this.resetarCidades();
+          this.carregandoCidades$.next(true);
+        }), switchMap(uf => {
+          if (uf) {
+            return this.ibgeService.getCidadesPorEstado(uf).pipe(
+              tap(() => this.carregandoCidades$.next(false)));
+          }
+          return of([]);
+        })
+      )
+    }
+  }
+
+  private resetarCidades() {
+    this.dadosPessoaisForm.get('cidade')?.setValue('');
+  }
+
 }
 
